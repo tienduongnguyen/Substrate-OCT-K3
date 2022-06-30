b97bdd46cf5250ff85861a136e66379799a854a6
Nguyen Tien Duong <ntduong1292@gmail.com>
2022-06-30 17:08:55 +0700

    finish exercise day 4

---
+++ day4/target/debug/day4.d
@@ -0,0 +1,1 @@
+/mnt/c/dylan/rust/Substrate-class/Substrate-OCT-K3/day4/target/debug/day4: /mnt/c/dylan/rust/Substrate-class/Substrate-OCT-K3/day4/src/main.rs
---
+++ day4/target/debug/deps/day4-5092401953cc804d.d
@@ -0,0 +1,5 @@
+/mnt/c/dylan/rust/Substrate-class/Substrate-OCT-K3/day4/target/debug/deps/day4-5092401953cc804d: src/main.rs
+
+/mnt/c/dylan/rust/Substrate-class/Substrate-OCT-K3/day4/target/debug/deps/day4-5092401953cc804d.d: src/main.rs
+
+src/main.rs:
